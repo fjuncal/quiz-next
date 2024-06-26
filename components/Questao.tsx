ee6168a1224b03787ac296063fc77d45ad318391
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-26 17:29:36 -0300

    Crando metodo de renderizar respostas

--- components/Questao.tsx
+++ components/Questao.tsx
@@ -1,14 +1,24 @@
 import QuestaoModel from "@/model/questao";
 import styles from "../styles/Questao.module.css";
 import Enunciado from "./Enunciado";
+import Resposta from "./Resposta";
 interface QuestaoProps {
   valor: QuestaoModel;
 }
 export default function Questao(props: QuestaoProps) {
   const questao = props.valor;
+
+  function renderRespostas() {
+    return questao.respostas!.map((resposta, i) => {
+      return (
+        <Resposta valor={resposta} indice={i} letra="A" corLetra="#F2C866" />
+      );
+    });
+  }
   return (
     <div className={styles.questao}>
       <Enunciado texto={questao.enunciado!} />
+      {renderRespostas()}
     </div>
   );
 }
