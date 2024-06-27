53bf8459a5389a6356c08bded92d1968a0194ff1
Fellipe Juncal <fjuncal7@gmail.com>
2024-06-27 20:08:06 -0300

    Implementando css no componente de estatistica e resultado e colocando botão de tentar novamente

--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import styles from "../styles/Resultado.module.css";
 import Estatistica from "@/components/Estatistica";
+import Botao from "@/components/Botao";
 
 export default function resultado() {
   const router = useRouter();
@@ -11,9 +12,16 @@ export default function resultado() {
   return (
     <div className={styles.resultado}>
       <h1>Resultado Final</h1>
-      <Estatistica texto="Perguntas" valor={total} />
-      <Estatistica texto="Certas" valor={certas} />
-      <Estatistica texto="Percentual" valor={`${percentual}%`} />
+      <div style={{ display: "flex" }}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9cd6a4" />
+        <Estatistica
+          texto="Percentual"
+          valor={`${percentual}%`}
+          corFundo="#DE6A33"
+        />
+      </div>
+      <Botao href="/" texto="Tentar Novamente" />
     </div>
   );
 }
